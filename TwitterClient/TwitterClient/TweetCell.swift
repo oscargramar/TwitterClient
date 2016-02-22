d954b6ee39915ad4c44509c052f52ba2e342e245
Oscar G.M <oscar@wustl.edu>
2016-02-21 21:43:32 -0600

    custom cells

---
+++ TwitterClient/TwitterClient/TweetCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetTableViewCell.swift
+//  TwitterClient
+//
+//  Created by Oscar G.M on 2/21/16.
+//  Copyright © 2016 GMStudios. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
